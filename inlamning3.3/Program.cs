df569513c0b1e08638b3b734b33730732d04d156
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-11 11:03:22 +0200

    Add project files.

---
+++ inlamning3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlamning3._3
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hur många hela timmar vill du hyra en bil?");
+            short hours = short.Parse(Console.ReadLine());
+            int cost = hours * 80;
+
+            if (cost <= 950)
+                Console.WriteLine("Det kommer kosta " + cost + " kr");
+            else
+                Console.WriteLine("Det kommer att kosta 950 kr");
+
+            Console.ReadKey();
+        
+        }
+    
+    }
+
+}
